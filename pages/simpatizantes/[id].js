c0fef3e62a7144ea1bed10bb366d2d144d9bbaaf
Diego <drbv27@gmail.com>
2022-02-13 15:29:31 -0500

    puting the info into the state

--- pages/simpatizantes/[id].js
+++ pages/simpatizantes/[id].js
@@ -1,16 +1,52 @@
-import React, { useEffect, useLayoutEffect } from "react";
+import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
+import { doc, getDoc, getFirestore } from "firebase/firestore";
+
+import { FirebaseContext } from "../../firebase";
 
 const Simpatizante = () => {
+  //State del componente
+  const [simpatizante, setSimpatizante] = useState({});
   //Routing para obtener el id actual
   const router = useRouter();
   const {
     query: { id },
   } = router;
 
+  //context de firebase
+  const { firebase } = useContext(FirebaseContext);
+  const db = getFirestore();
+
   useEffect(() => {
     if (id) {
-      console.log("Ya hay un Id", id);
+      const obtenerSimpatizante = async () => {
+        const docRef = doc(db, "pruebas", id);
+        const docSnap = await getDoc(docRef);
+        const {
+          cedula,
+          nombre,
+          direccion,
+          telefono,
+          comuna,
+          puesto,
+          lider,
+          creado,
+        } = docSnap.data().simpatizante;
+        const datos = {
+          cedula,
+          nombre,
+          direccion,
+          telefono,
+          comuna,
+          puesto,
+          lider,
+          creado,
+        };
+        /*  console.log(docSnap.data().simpatizante.nombre); */
+        /* console.log(datos); */
+        setSimpatizante(datos);
+      };
+      obtenerSimpatizante();
     }
   }, [id]);
 
