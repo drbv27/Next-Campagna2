d4813c9723b464d82c7dadf57d75c9f6c8b04d42
Diego <drbv27@gmail.com>
2022-02-13 17:33:56 -0500

    validating if a simpatizante exist

---
+++ components/layout/404.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Error404 = () => {
+  return (
+    <>
+      <h1>404</h1>
+      <h1>Simpatizante No existente</h1>
+    </>
+  );
+};
+
+export default Error404;
--- pages/simpatizantes/[id].js
+++ pages/simpatizantes/[id].js
@@ -1,12 +1,21 @@
 import React, { useEffect, useContext, useState } from "react";
 import { useRouter } from "next/router";
 import { doc, getDoc, getFirestore } from "firebase/firestore";
+import styled from "@emotion/styled";
+
+import Layout from "../../components/layout/Layout";
 
 import { FirebaseContext } from "../../firebase";
+import Error404 from "../../components/layout/404";
+
+const Principal = styled.div`
+  margin-left: 200px;
+`;
 
 const Simpatizante = () => {
   //State del componente
   const [simpatizante, setSimpatizante] = useState({});
+  const [error, setError] = useState(false);
   //Routing para obtener el id actual
   const router = useRouter();
   const {
@@ -22,35 +31,51 @@ const Simpatizante = () => {
       const obtenerSimpatizante = async () => {
         const docRef = doc(db, "pruebas", id);
         const docSnap = await getDoc(docRef);
-        const {
-          cedula,
-          nombre,
-          direccion,
-          telefono,
-          comuna,
-          puesto,
-          lider,
-          creado,
-        } = docSnap.data().simpatizante;
-        const datos = {
-          cedula,
-          nombre,
-          direccion,
-          telefono,
-          comuna,
-          puesto,
-          lider,
-          creado,
-        };
+        if (docSnap.exists()) {
+          console.log("producto existe");
+          const {
+            cedula,
+            nombre,
+            direccion,
+            telefono,
+            comuna,
+            puesto,
+            lider,
+            creado,
+          } = docSnap.data().simpatizante;
+          const datos = {
+            cedula,
+            nombre,
+            direccion,
+            telefono,
+            comuna,
+            puesto,
+            lider,
+            creado,
+          };
+          setSimpatizante(datos);
+        } else {
+          console.log("producto NO existe");
+          setError(true);
+        }
+
         /*  console.log(docSnap.data().simpatizante.nombre); */
         /* console.log(datos); */
-        setSimpatizante(datos);
       };
       obtenerSimpatizante();
     }
   }, [id]);
 
-  return <h1>Desde {id}</h1>;
+  /*  if (error) return <Error404 />;
+  return <h1>Desde {id}</h1>; */
+
+  return (
+    <div>
+      <Layout>
+        <Principal>{error ? <Error404 /> : <h1>Desde {id}</h1>}</Principal>
+      </Layout>
+    </div>
+  );
 };
 
 export default Simpatizante;
