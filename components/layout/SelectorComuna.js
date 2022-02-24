bda3357e2a640d5db6c04bf2e4f44b60d1140a30
Diego <drbv27@gmail.com>
2022-02-24 16:08:57 -0500

    solution select

--- components/layout/SelectorComuna.js
+++ components/layout/SelectorComuna.js
@@ -1,6 +1,8 @@
 import styled from "@emotion/styled";
 
 const SelectorComuna = styled.select`
+  display: inline-block;
+  width: 90%;
   padding: 0.5rem;
   margin-left: 1rem;
   border-radius: 5px;
