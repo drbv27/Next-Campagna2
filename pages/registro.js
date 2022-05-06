f793f051c44a9121e975b1e9c5dad0c59fe6fc02
Diego Bonilla <drbv27@gmail.com>
2022-05-06 16:48:35 -0500

    changing register form

--- pages/registro.js
+++ pages/registro.js
@@ -1,8 +1,17 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useContext, useRef } from "react";
+import { comunas } from "../data/comunas";
 import Layout from "../components/layout/Layout";
 import { css } from "@emotion/react";
 import { useRouter } from "next/router";
 import styled from "@emotion/styled";
+import InputRegistro from "../components/ui/InputRegistro";
+import SelectorComuna from "../components/layout/SelectorComuna";
+import {
+  DatosPersonales,
+  InfoImpLab,
+  CentrarForm,
+  Principal,
+} from "../components/layout/FormRegistro";
 import {
   Formulario,
   Formulario2,
@@ -31,37 +40,6 @@ import { ref, getDownloadURL, uploadBytesResumable } from "@firebase/storage";
 import useValidacion from "../hooks/useValidacion";
 import validarCrearSimpatizante from "../validacion/validarCrearSimpatizante";
 
-const Principal = styled.div`
-  margin-left: 200px;
-  text-align: center;
-`;
-
-const Flex = styled.div`
-  display: flex;
-  justify-content: space-evenly;
-  margin-bottom: 1rem;
-`;
-
-const MyInput = styled.div`
-  display: flex;
-  margin-top: 0.8rem;
-  label {
-    font-size: 1rem;
-  }
-  input {
-    flex: 1;
-    padding: 0.2rem;
-    margin-left: 1rem;
-  }
-`;
-
-const Grupo = styled.fieldset`
-  flex: 1;
-  background-color: #fafafa;
-  border: 1px solid #e1e1e1;
-  text-align: left;
-`;
-
 const STATE_INICIAL = {
   cedula: "",
   nombre: "",
@@ -73,156 +51,144 @@ const STATE_INICIAL = {
 };
 
 const Registro = () => {
-  const router = useRouter();
-  const [error, guardarError] = useState(false);
-
-  const { valores, errores, submitForm, handleSubmit, handleChange } =
-    useValidacion(STATE_INICIAL, validarCrearSimpatizante, crearSimpatizante);
-
-  const { cedula, nombre, direccion, telefono, comuna, puesto, lider } =
-    valores;
-
   //context con operaciones crud de firebase
   const { usuario, firebase } = useContext(FirebaseContext);
+  console.log(comunas);
+  const [idPuestos, setIdPuestos] = useState(-1);
+  const [simpatizante, setSimpatizante] = useState({});
 
-  async function crearSimpatizante() {
+  const handleSetPuestos = function (e) {
+    const opcion = e.target.value;
+    console.log("comuna", e.target.value);
+    setIdPuestos(opcion);
+  };
+  async function submit(e) {
+    e.preventDefault();
+    const db = getFirestore();
+    const form = new FormData();
+    form.append("cedula", cedula.current.value);
+    form.append("nombre", nombre.current.value);
+    form.append("direccion", direccion.current.value);
+    form.append("telefono", telefono.current.value);
+    form.append("comuna", comuna.current.value);
+    form.append("puesto", puesto.current.value);
+    form.append("lider", lider.current.value);
+    const creado = Date.now();
+    form.append("creado", creado);
+    const data = Array.from(form);
+    const simpatizante = Object.fromEntries(data);
+    /* console.log(simpatizante); */
+    setSimpatizante(simpatizante);
     //si el usuario no esta autenticado llevar al login
     if (!usuario) {
       return router.push("/login");
     }
-    //crear el onjeto de nuevo simpatizante
-    const simpatizante = {
-      cedula,
-      nombre,
-      direccion,
-      telefono,
-      comuna,
-      puesto,
-      lider,
-      creado: Date.now(),
-    };
-    console.log(simpatizante);
     //insertar en la base de datos
-    const db = getFirestore();
-    /*     const docRef = await addDoc(collection(db, "simpatizantes"), {
-      simpatizante,
-    }); */
+    console.log(simpatizante.cedula);
+    console.log("simpatizante:", simpatizante);
 
-    await setDoc(doc(db, "pruebas", cedula), { simpatizante });
+    await setDoc(doc(db, "pruebas", simpatizante.cedula), { simpatizante });
   }
+  const cedula = useRef();
+  const nombre = useRef();
+  const direccion = useRef();
+  const telefono = useRef();
+  const comuna = useRef();
+  const puesto = useRef();
+  const lider = useRef();
+  const router = useRouter();
+  const [error, guardarError] = useState(false);
+
+  /*  const { valores, errores, submitForm, handleSubmit, handleChange } =
+    useValidacion(STATE_INICIAL, validarCrearSimpatizante, crearSimpatizante); */
+
+  /* const { cedula, nombre, direccion, telefono, comuna, puesto, lider } =
+    valores; */
 
   return (
     <div>
       <Layout>
         <Principal>
-          <>
-            <h1
-              css={css`
-                margin-top: 2rem;
-                color: #1097d5;
-              `}
-            >
-              Registro Simpatizantes
-            </h1>
-            <form onSubmit={handleSubmit} noValidate>
-              <Flex>
-                <Grupo>
-                  <legend>Datos Personales</legend>
-                  <MyInput>
-                    <label htmlFor="cedula">Cédula: </label>
-                    <input
-                      type="number"
-                      id="cedula"
-                      placeholder="Ingresa la cédula"
-                      name="cedula"
-                      value={cedula}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.cedula && <Error>{errores.cedula}</Error>}
-                  <MyInput>
-                    <label htmlFor="nombre">Nombre Completo: </label>
-                    <input
-                      type="text"
-                      id="nombre"
-                      placeholder="Ingresa Nombre y Apellidos"
-                      name="nombre"
-                      value={nombre}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.nombre && <Error>{errores.nombre}</Error>}
-                  <MyInput>
-                    <label htmlFor="direccion">Dirección: </label>
-                    <input
-                      type="text"
-                      id="direccion"
-                      placeholder="Ingresa La Direccion"
-                      name="direccion"
-                      value={direccion}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.direccion && <Error>{errores.direccion}</Error>}
-                  <MyInput>
-                    <label htmlFor="telefono">Teléfono: </label>
-                    <input
-                      type="number"
-                      id="telefono"
-                      placeholder="Ingresa Numero de telefono"
-                      name="telefono"
-                      value={telefono}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-
-                  {errores.telefono && <Error>{errores.telefono}</Error>}
-                </Grupo>
-                <Grupo>
-                  <legend>Información Electoral</legend>
-                  <MyInput>
-                    <label htmlFor="comuna">Comúna: </label>
-                    {/* <Select /> */}
-                    <input
-                      type="text"
-                      id="comuna"
-                      placeholder="Ingresa La Comuna"
-                      name="comuna"
-                      value={comuna}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.comuna && <Error>{errores.comuna}</Error>}
-                  <MyInput>
-                    <label htmlFor="puesto">Puesto de Votación </label>
-                    <input
-                      type="text"
-                      id="puesto"
-                      placeholder="Ingresa el puesto"
-                      name="puesto"
-                      value={puesto}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.puesto && <Error>{errores.puesto}</Error>}
-                  <MyInput>
-                    <label htmlFor="lider">Lider </label>
-                    <input
-                      type="text"
-                      id="lider"
-                      placeholder="Ingresa Nombre y Apellidos Lider"
-                      name="lider"
-                      value={lider}
-                      onChange={handleChange}
-                    />
-                  </MyInput>
-                  {errores.lider && <Error>{errores.lider}</Error>}
-                </Grupo>
-              </Flex>
-              {error && <Error>{error}</Error>}
-              <InputSubmit type="submit" value="Registrar Simpatizante" />
+          <h1
+            css={css`
+              margin-top: 2rem;
+              color: #1097d5;
+            `}
+          >
+            Registro Simpatizantes 21
+          </h1>
+          <CentrarForm>
+            <form onSubmit={submit}>
+              <DatosPersonales>
+                <h2>Datos personales</h2>
+                <InfoImpLab>
+                  {/*  <label htmlFor="cedula">Cédula:</label> */}
+                  <InputRegistro
+                    type="number"
+                    name="cedula"
+                    placeholder="Cedúla"
+                    ref={cedula}
+                  />
+                </InfoImpLab>
+                <InfoImpLab>
+                  {/* <label htmlFor="nombre">Nombre Completo:</label> */}
+                  <InputRegistro
+                    type="text"
+                    name="nombre"
+                    placeholder="Nombre y Apellido"
+                    ref={nombre}
+                  />
+                </InfoImpLab>
+                <InfoImpLab>
+                  {/* <label htmlFor="direccion">Dirección::</label> */}
+                  <InputRegistro
+                    type="text"
+                    name="direccion"
+                    placeholder="Dirección"
+                    ref={direccion}
+                  />
+                </InfoImpLab>
+                <InfoImpLab>
+                  {/* <label htmlFor="telefono">Telefono:</label> */}
+                  <InputRegistro
+                    type="text"
+                    name="telefono"
+                    placeholder="Teléfono"
+                    ref={telefono}
+                  />
+                </InfoImpLab>
+                <SelectorComuna
+                  name="comuna"
+                  id="comuna"
+                  onChange={handleSetPuestos}
+                  ref={comuna}
+                >
+                  <option value={-1}>Seleccione Comuna</option>
+                  {comunas.map((item, i) => (
+                    <option key={"comuna" + (i + 1)} value={i}>
+                      {item.nombre}
+                    </option>
+                  ))}
+                </SelectorComuna>
+                <SelectorComuna name="puesto" id="puesto" ref={puesto}>
+                  <option value={-1}>Seleccione Puesto Votacion</option>
+                  {idPuestos > -1 &&
+                    comunas[idPuestos].puestos.map((item, i) => (
+                      <option key={"puesto" + (i + 1)} value={i}>
+                        {item}
+                      </option>
+                    ))}
+                </SelectorComuna>
+                <InputRegistro
+                  type="text"
+                  placeholder="Lider"
+                  name="lider"
+                  ref={lider}
+                />
+                <button type="submit">Enviar</button>
+              </DatosPersonales>
             </form>
-          </>
+          </CentrarForm>
         </Principal>
       </Layout>
     </div>
---
+++ pages/registro4.js
@@ -0,0 +1,232 @@
+import React, { useState, useContext } from "react";
+import Layout from "../components/layout/Layout";
+import { css } from "@emotion/react";
+import { useRouter } from "next/router";
+import styled from "@emotion/styled";
+import {
+  Formulario,
+  Formulario2,
+  Campo,
+  Campo2,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
+
+import Select from "../components/ui/Select";
+
+import { FirebaseContext } from "../firebase";
+import firebase from "../firebase/firebase";
+
+import {
+  collection,
+  addDoc,
+  doc,
+  setDoc,
+  getFirestore,
+} from "firebase/firestore";
+
+import { ref, getDownloadURL, uploadBytesResumable } from "@firebase/storage";
+
+//validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearSimpatizante from "../validacion/validarCrearSimpatizante";
+
+const Principal = styled.div`
+  margin-left: 200px;
+  text-align: center;
+`;
+
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  margin-bottom: 1rem;
+`;
+
+const MyInput = styled.div`
+  display: flex;
+  margin-top: 0.8rem;
+  label {
+    font-size: 1rem;
+  }
+  input {
+    flex: 1;
+    padding: 0.2rem;
+    margin-left: 1rem;
+  }
+`;
+
+const Grupo = styled.fieldset`
+  flex: 1;
+  background-color: #fafafa;
+  border: 1px solid #e1e1e1;
+  text-align: left;
+`;
+
+const STATE_INICIAL = {
+  cedula: "",
+  nombre: "",
+  direccion: "",
+  telefono: "",
+  comuna: "",
+  puesto: "",
+  lider: "",
+};
+
+const Registro = () => {
+  const router = useRouter();
+  const [error, guardarError] = useState(false);
+
+  const { valores, errores, submitForm, handleSubmit, handleChange } =
+    useValidacion(STATE_INICIAL, validarCrearSimpatizante, crearSimpatizante);
+
+  const { cedula, nombre, direccion, telefono, comuna, puesto, lider } =
+    valores;
+
+  //context con operaciones crud de firebase
+  const { usuario, firebase } = useContext(FirebaseContext);
+
+  async function crearSimpatizante() {
+    //si el usuario no esta autenticado llevar al login
+    if (!usuario) {
+      return router.push("/login");
+    }
+    //crear el onjeto de nuevo simpatizante
+    const simpatizante = {
+      cedula,
+      nombre,
+      direccion,
+      telefono,
+      comuna,
+      puesto,
+      lider,
+      creado: Date.now(),
+    };
+    console.log(simpatizante);
+    //insertar en la base de datos
+    const db = getFirestore();
+    /*     const docRef = await addDoc(collection(db, "simpatizantes"), {
+      simpatizante,
+    }); */
+
+    await setDoc(doc(db, "pruebas", cedula), { simpatizante });
+  }
+
+  return (
+    <div>
+      <Layout>
+        <Principal>
+          <>
+            <h1
+              css={css`
+                margin-top: 2rem;
+                color: #1097d5;
+              `}
+            >
+              Registro Simpatizantes
+            </h1>
+            <form onSubmit={handleSubmit} noValidate>
+              <Flex>
+                <Grupo>
+                  <legend>Datos Personales</legend>
+                  <MyInput>
+                    <label htmlFor="cedula">Cédula: </label>
+                    <input
+                      type="number"
+                      id="cedula"
+                      placeholder="Ingresa la cédula"
+                      name="cedula"
+                      value={cedula}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.cedula && <Error>{errores.cedula}</Error>}
+                  <MyInput>
+                    <label htmlFor="nombre">Nombre Completo: </label>
+                    <input
+                      type="text"
+                      id="nombre"
+                      placeholder="Ingresa Nombre y Apellidos"
+                      name="nombre"
+                      value={nombre}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.nombre && <Error>{errores.nombre}</Error>}
+                  <MyInput>
+                    <label htmlFor="direccion">Dirección: </label>
+                    <input
+                      type="text"
+                      id="direccion"
+                      placeholder="Ingresa La Direccion"
+                      name="direccion"
+                      value={direccion}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.direccion && <Error>{errores.direccion}</Error>}
+                  <MyInput>
+                    <label htmlFor="telefono">Teléfono: </label>
+                    <input
+                      type="number"
+                      id="telefono"
+                      placeholder="Ingresa Numero de telefono"
+                      name="telefono"
+                      value={telefono}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+
+                  {errores.telefono && <Error>{errores.telefono}</Error>}
+                </Grupo>
+                <Grupo>
+                  <legend>Información Electoral</legend>
+                  <MyInput>
+                    <label htmlFor="comuna">Comúna: </label>
+                    {/* <Select /> */}
+                    <input
+                      type="text"
+                      id="comuna"
+                      placeholder="Ingresa La Comuna"
+                      name="comuna"
+                      value={comuna}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.comuna && <Error>{errores.comuna}</Error>}
+                  <MyInput>
+                    <label htmlFor="puesto">Puesto de Votación </label>
+                    <input
+                      type="text"
+                      id="puesto"
+                      placeholder="Ingresa el puesto"
+                      name="puesto"
+                      value={puesto}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.puesto && <Error>{errores.puesto}</Error>}
+                  <MyInput>
+                    <label htmlFor="lider">Lider </label>
+                    <input
+                      type="text"
+                      id="lider"
+                      placeholder="Ingresa Nombre y Apellidos Lider"
+                      name="lider"
+                      value={lider}
+                      onChange={handleChange}
+                    />
+                  </MyInput>
+                  {errores.lider && <Error>{errores.lider}</Error>}
+                </Grupo>
+              </Flex>
+              {error && <Error>{error}</Error>}
+              <InputSubmit type="submit" value="Registrar Simpatizante" />
+            </form>
+          </>
+        </Principal>
+      </Layout>
+    </div>
+  );
+};
+
+export default Registro;
