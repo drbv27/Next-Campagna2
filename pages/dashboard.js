10137843907f87f6db4d750563cd1a044c1132c1
Diego Bonilla <drbv27@gmail.com>
2022-05-09 17:00:56 -0500

    barchart

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -2,6 +2,7 @@ import Layout from "../components/layout/Layout";
 import styled from "@emotion/styled";
 import Image from "next/image";
 import LineChart from "../components/layout/LineChart";
+import BarChart from "../components/layout/BarChart";
 
 const Principal = styled.div`
   margin-left: 200px;
@@ -16,6 +17,8 @@ export default function Home() {
         <Principal>
           <h1>Dashboard</h1>
           <LineChart/>
+          <hr/>
+          <BarChart/>
         </Principal>
       </Layout>
     </div>
